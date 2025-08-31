429cf2d5c1b40fbb98a9e03ffdb47cec78f33f6a
adehad <26027314+adehad@users.noreply.github.com>
2025-08-31 02:34:11 +0100

    lint

--- src/components/base-recitation.ts
+++ src/components/base-recitation.ts
@@ -1,34 +1,5 @@
 import { css, LitElement } from "lit";
 
 export class BaseRecitation extends LitElement {
-  static styles = css`
-    .title {
-      font-size: 1.5em;
-      font-weight: bold;
-      margin-bottom: 1em;
-    }
-
-    .instruction {
-      font-style: italic;
-      margin-bottom: 1em;
-    }
-
-    .entry {
-      margin-bottom: 1em;
-    }
-
-    .arabic {
-      font-family: 'Scheherazade New', serif;
-      font-size: 2.2em;
-      text-align: right;
-    }
-
-    .translit {
-      font-style: italic;
-    }
-
-    .translation {
-      font-weight: bold;
-    }
-  `;
+  static styles = [css``];
 }
