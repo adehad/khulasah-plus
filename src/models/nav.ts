63e800648dea6f8cf954258b676f9e1f404048c2
adehad <26027314+adehad@users.noreply.github.com>
2026-03-16 19:44:59 +0000

    internal review pass

--- src/models/nav.ts
+++ src/models/nav.ts
@@ -1,6 +1,3 @@
-import type { TemplateResult } from "lit";
-import { html } from "lit";
-
 export class NavModel {
   href: string;
   title: string;
@@ -8,9 +5,5 @@ export class NavModel {
   constructor({ href, title }: { href: string; title: string }) {
     this.href = href;
     this.title = title;
-  }
-
-  render(): TemplateResult {
-    return html`<nav-button href=${this.href}>${this.title}</nav-button>`;
   }
 }
